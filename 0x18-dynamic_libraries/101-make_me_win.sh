3320442af09b309fccb04ebc5a8b56a468c49d0e
rimchafik <rimchafik10@gmail.com>
2024-01-15 14:40:14 -0800

    Tasks Dynamic libs

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/rimchafik/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
+wget -P /tmp https://github.com/rimchafik/alx/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
